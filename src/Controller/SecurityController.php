f426f1286a57dc1d147354fec691d5429f13caf5
loic-maufroid <maufroid.l@hotmail.fr>
2020-03-28 22:49:47 +0100

    register : formulaire et vue

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder){
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -32,5 +43,38 @@ class SecurityController extends AbstractController
     public function logout()
     {
        
+    }
+
+    /**
+     * @Route("/register",name="app_register")
+     */
+    public function register(Request $request){
+
+        $user = new User();
+
+        $form = $this->createForm(UserType::class,$user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $user->setPassword($this->passwordEncoder->encodePassword($user,$user->getPassword()));
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($user);
+
+            $entityManager->flush();
+
+            $this->addFlash('success','Bienvenue '.$user->getEmail().' sur notre site E-commerce ! Connectez vous dès maintenant !');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render("security/register.html.twig",[
+            "form" => $form->createView()
+        ]);
+
     }
 }
